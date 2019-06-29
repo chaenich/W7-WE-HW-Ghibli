f02c211d65051785324ccdfc1ed0c84c501b60b8
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-29 11:57:08 +0100

    Added navbar to select all-favourites-watched

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template lang="html">
   <div>
+    <nav-bar></nav-bar>
     <router-view v-bind:films="films" v-bind:favourites="favourites" v-bind:watched="watched"></router-view>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar"
 
 export default {
   name: "app",
@@ -19,6 +21,9 @@ export default {
     fetch("https://ghibliapi.herokuapp.com/films")
     .then(response => response.json())
     .then(data => this.films = data)
+  },
+  components: {
+    "nav-bar": NavBar
   }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <router-link v-bind:to="{ name: 'film-view' }">All Films</router-link> |
+    <router-link v-bind:to="{ name: 'favourite-view' }">Favourites</router-link> | 
+    <router-link v-bind:to="{ name: 'watched-view' }">Watched already</router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: "nav-bar"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
